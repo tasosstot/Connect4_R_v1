8693c27cc2cfabe242bcaa4bb26780931c0ee95a
tasosstot <70763683+tasosstot@users.noreply.github.com>
2021-02-07 23:55:42 +0200

    Update MiniMaxAi.java
    Cognitive Complexity

--- src/connect4/MiniMaxAi.java
+++ src/connect4/MiniMaxAi.java
@@ -43,49 +43,47 @@ public class MiniMaxAi {
 
 	    // Initiates the MiniMax algorithm
 		public Move miniMax(Board board) {
-	        // If P1 plays then it wants to MAXimize the heuristics value.
+	        
 	        if (aiPlayer == Constants.P1) {
 	            return max(new Board(board), 0);
 	        }
-	        // If P2 plays then it wants to MINimize the heuristics value.
+	        
 	        else {
 	            return min(new Board(board), 0);
 	        }
 		}
 
-	    // The max and min functions are called interchangeably, one after another until a max depth is reached
+	  
 		public Move max(Board board, int depth) {
 	        Random r = new Random();
 
-	        /* If MAX is called on a state that is terminal or after a maximum depth is reached,
-	         * then a heuristic is calculated on the state and the move returned.
-	         */
+	      
 			if((board.checkForGameOver()) || (depth == maxDepth)) {
 				Move lastMove = new Move(board.getLastMove().getRow(), board.getLastMove().getColumn(), board.evaluate());
 				return lastMove;
 			}
-	        // The children-moves of the state are calculated
-			ArrayList<Board> children = new ArrayList<Board>(board.getChildren(Constants.P1));
+
+			ArrayList<Board> children = new ArrayList<>(board.getChildren(Constants.P1));
 			Move maxMove = new Move(Integer.MIN_VALUE);
 			for (Board child : children) {
 	            // And for each child min is called, on a lower depth
 				Move move = min(child, depth + 1);
 	            // The child-move with the greatest value is selected and returned by max
-				if (move.getValue() >= maxMove.getValue()) {
-	                if ((move.getValue() == maxMove.getValue())) {
-	                    // If the heuristic has the same value then we randomly choose one of the two moves
-	                    if (r.nextInt(2) == 0) {
-	                        maxMove.setRow(child.getLastMove().getRow());
-	                        maxMove.setColumn(child.getLastMove().getColumn());
-	                        maxMove.setValue(move.getValue());
-	                    }
-	                }
-	                else {
-	                    maxMove.setRow(child.getLastMove().getRow());
-	                    maxMove.setColumn(child.getLastMove().getColumn());
-	                    maxMove.setValue(move.getValue());
-	                }
-				}
+                if((move.getValue() == maxMove.getValue()) && (r.nextInt(2) == 0)){
+                    maxMove.setRow(child.getLastMove().getRow());
+                    maxMove.setColumn(child.getLastMove().getColumn());
+                    maxMove.setValue(move.getValue());
+                }
+                else if(move.getValue() >= maxMove.getValue()){
+                    maxMove.setRow(child.getLastMove().getRow());
+                    maxMove.setColumn(child.getLastMove().getColumn());
+                    maxMove.setValue(move.getValue());
+                }
+
+
+	
+
+
 			}
 			return maxMove;
 		}
@@ -98,7 +96,7 @@ public class MiniMaxAi {
 				Move lastMove = new Move(board.getLastMove().getRow(), board.getLastMove().getColumn(), board.evaluate());
 				return lastMove;
 			}
-			ArrayList<Board> children = new ArrayList<Board>(board.getChildren(Constants.P2));
+			ArrayList<Board> children = new ArrayList<>(board.getChildren(Constants.P2));
 			Move minMove = new Move(Integer.MAX_VALUE);
 			for (Board child : children) {
 				Move move = max(child, depth + 1);
@@ -146,27 +144,24 @@ public class MiniMaxAi {
 				return lastMove;
 			}
 	        // The children-moves of the state are calculated
-			ArrayList<Board> children = new ArrayList<Board>(board.getChildren(Constants.P1));
+			ArrayList<Board> children = new ArrayList<>(board.getChildren(Constants.P1));
 			Move maxMove = new Move(Integer.MIN_VALUE);
 			for (Board child : children) {
 	            // And for each child min is called, on a lower depth.
 				Move move = minAlphaBeta(child, depth + 1, a, b);
 	            // The child-move with the greatest value is selected and returned by max.
-				if (move.getValue() >= maxMove.getValue()) {
-	                if ((move.getValue() == maxMove.getValue())) {
-	                    // If the heuristic has the same value, then we randomly choose one of the two moves.
-	                    if (r.nextInt(2) == 0) {
-	                        maxMove.setRow(child.getLastMove().getRow());
-	                        maxMove.setColumn(child.getLastMove().getColumn());
-	                        maxMove.setValue(move.getValue());
-	                    }
-	                }
-	                else {
-	                    maxMove.setRow(child.getLastMove().getRow());
-	                    maxMove.setColumn(child.getLastMove().getColumn());
-	                    maxMove.setValue(move.getValue());
-	                }
-				}
+
+
+                if((move.getValue() == maxMove.getValue()) & (r.nextInt(2) == 0)){
+                    maxMove.setRow(child.getLastMove().getRow());
+                    maxMove.setColumn(child.getLastMove().getColumn());
+                    maxMove.setValue(move.getValue());
+                }
+                else if(move.getValue() >= maxMove.getValue()){
+                    maxMove.setRow(child.getLastMove().getRow());
+                    maxMove.setColumn(child.getLastMove().getColumn());
+                    maxMove.setValue(move.getValue());
+                }
 				
 				// Beta pruning.
 				if (maxMove.getValue() >= b) {
@@ -188,32 +183,27 @@ public class MiniMaxAi {
 				Move lastMove = new Move(board.getLastMove().getRow(), board.getLastMove().getColumn(), board.evaluate());
 				return lastMove;
 			}
-			ArrayList<Board> children = new ArrayList<Board>(board.getChildren(Constants.P2));
+			ArrayList<Board> children = new ArrayList<>(board.getChildren(Constants.P2));
 			Move minMove = new Move(Integer.MAX_VALUE);
 			for (Board child : children) {
 				Move move = maxAlphaBeta(child, depth + 1, a, b);
-				if(move.getValue() <= minMove.getValue()) {
-	                if ((move.getValue() == minMove.getValue())) {
-	                    if (r.nextInt(2) == 0) {
-	                        minMove.setRow(child.getLastMove().getRow());
-	                        minMove.setColumn(child.getLastMove().getColumn());
-	                        minMove.setValue(move.getValue());
-	                    }
-	                }
-	                else {
-	                        minMove.setRow(child.getLastMove().getRow());
-	                        minMove.setColumn(child.getLastMove().getColumn());
-	                        minMove.setValue(move.getValue());
-	                }
-	            }
-				
-				// Alpha pruning
+                if((move.getValue() == minMove.getValue()) && (r.nextInt(2) == 0)){
+                    minMove.setRow(child.getLastMove().getRow());
+                    minMove.setColumn(child.getLastMove().getColumn());
+                    minMove.setValue(move.getValue());                   
+                }
+                else if(move.getValue() <= minMove.getValue()){
+                    minMove.setRow(child.getLastMove().getRow());
+                    minMove.setColumn(child.getLastMove().getColumn());
+                    minMove.setValue(move.getValue());
+                }
+
 				if (minMove.getValue() <= a) {
-					// System.out.println("Alpha pruning: " + a);
+					
 					return minMove;
 				}
 				
-	            // Update the b of the current min node.
+	            
 				b = (b < minMove.getValue()) ? b : minMove.getValue();
 	        }
 	        return minMove;
